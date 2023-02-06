960222438b15e3392e860534345fce24e69bb47c
techyhk <kumar.himanshu@icloud.com>
2023-02-06 15:47:23 +0530

    similarwebData

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,19 @@ import { useUser } from "../lib/hooks";
 import Layout from "../components/layout/Layout";
 import Details from "../components/details";
 import axios from "axios";
+import { SSRProvider } from 'react-bootstrap';
 
 let socialUrls = {};
 
 const Home = () => {
-  const user = useUser();
+  const user = useUser({ redirectTo: "/login" });
   const [search, setSearch] = React.useState("");
   const [enable, setEnable] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
   const [data, setData] = React.useState({});
   const [error, setError] = React.useState("");
   const [inputState, setInputState] = React.useState("enabled");
+  const [similarWebData, setSimilarWebData] = React.useState(null);
 
   const searchHandler = (e) => {
     setSearch(e.target.value);
@@ -23,58 +25,74 @@ const Home = () => {
     setError(null);
   };
 
-  return (
-    <Layout>
-      <Spacer y={4} />
-      <div css={{ maxHeight: "100vh" }}>
-        <Input
-          readOnly={inputState === "disabled" ? true : false}
-          className="search"
-          width="50%"
-          labelPlaceholder="Search"
-          clearable={inputState === "disabled" ? false : true}
-          bordered
-          color="warning"
-          onChange={searchHandler}
-          value={search}
-          aria-label="Search"
-        />
-        <Spacer y={0.5} />
-        <Button
-          onPress={async () => {
-            console.log("searchData", search);
-            setEnable(false);
-            setError(null);
-            setData({});
-            setLoading(true);
-            setInputState("disabled");
-            try {
-              socialUrls = await axios.post(`/api/pptr`, {
-                url: search,
-              });
-              setData(socialUrls.data);
-              setEnable(true);
-            } catch (err) {
-              setError(err.response.data.error);
-            } finally {
-              setLoading(false);
-              setInputState("enabled");
-            }
-          }}
-        >
-          Search
-        </Button>
+  async function getDataSimilarWeb(site) {
+    const res = await fetch(`https://data.similarweb.com/api/v1/data?domain=${site}`);
+    const data = await res.json();
+    setSimilarWebData(data);
+  }
 
-        {loading ? <Spacer y="5"><Loading type="points-opacity" size="xl" /><Text>Fetching Data....</Text></Spacer> : null}
-        {error ? <Text>{error}</Text> : null}
-        {enable ? (
-          <Details
-            site={search}
-            data={data}
-          />
-        ) : null}
-      </div>
-    </Layout>
+  return (
+    <SSRProvider>
+      {user && (
+        <Layout>
+          <Spacer y={4} />
+          <Text b h2>Enter Website</Text>
+          <div css={{ maxHeight: "100vh" }}>
+            <Input
+              readOnly={inputState === "disabled" ? true : false}
+              className="search"
+              width="50%"
+              Placeholder="Search"
+              clearable={inputState === "disabled" ? false : true}
+              color="warning"
+              onChange={searchHandler}
+              value={search}
+              aria-label="Search"
+              rounded
+            />
+            <Spacer y={0.5} />
+            <Button
+              color="gradient"
+              ghost
+              rounded
+              disabled={inputState === "disabled" ? true : false}
+              onPress={async () => {
+                console.log("searchData", search);
+                setEnable(false);
+                setError(null);
+                setData({});
+                setLoading(true);
+                setInputState("disabled");
+                await getDataSimilarWeb(search);
+                try {
+                  socialUrls = await axios.post(`/api/pptr`, {
+                    url: search,
+                  });
+                  setData(socialUrls.data);
+                  setEnable(true);
+                } catch (err) {
+                  setError(err.response.data.error);
+                } finally {
+                  setLoading(false);
+                  setInputState("enabled");
+                }
+              }}
+            >
+              Search
+            </Button>
+
+            {loading ? <Spacer y="5"><Loading type="points-opacity" size="xl" /></Spacer> : null}
+            {error ? <Text>Error :{error}</Text> : null}
+            {enable ? (
+              <Details
+                site={search}
+                data={data}
+                similarWebData={similarWebData}
+              />
+            ) : null}
+          </div>
+        </Layout>)}
+    </SSRProvider>
   );
 };
 
