e974578dd9109739be585cfc973f0fe1be8d78a4
techyhk <kumar.himanshu@icloud.com>
2023-02-06 15:57:06 +0530

    update navbar

--- components/navBar/NavBar.js
+++ components/navBar/NavBar.js
@@ -1,51 +1,12 @@
 import React from "react";
 import { Navbar, Button, Link, Text, useTheme, Avatar, Dropdown } from "@nextui-org/react";
 import { useUser } from "../../lib/hooks";
-import { icons } from "../icons/Icons.js";
 import { ThemeSwitch } from "./ThemeSwitch";
 import { useRouter } from "next/router";
 
 export function NavBar() {
   const user = useUser();
   const router = useRouter();
-
-  const { isDark } = useTheme();
-
-  const collapseItems = [
-    { name: "Profile", href: "/profile" },
-    { name: "AdsTxt", href: "/dashboard/adsTxt" },
-    { name: "Video Player", href: "/dashboard/videoPlayer" },
-    // { name: "Header Bidding", href: "/dashboard/headerBidding" },
-    { name: "Log Out", href: "/api/logout" },
-  ];
-
-  const menuItems = [
-    {
-      key: "Ads Txt",
-      name: "Ads Txt",
-      description: "UB Ads Txt",
-      href: "/dashboard/adsTxt",
-      icon: icons.server,
-    },
-    {
-      key: "Video Player",
-      name: "Video Player",
-      description: "UB Video Player",
-      href: "/dashboard/videoPlayer",
-      icon: icons.server,
-    },
-    // {
-    //   key: "Header Bidding",
-    //   name: "Header Bidding",
-    //   description: "UB Header Bidding",
-    //   href: "/dashboard/headerBidding",
-    //   icon: icons.activity,
-    // },
-  ];
-
-  const selectMenuItem = select => {
-    router.push(menuItems.find(item => select.has(item.key)).href);
-  };
 
   return (
     <>
@@ -102,28 +63,6 @@ export function NavBar() {
             </Navbar.Item>
           </Navbar.Content>
         )}
-        <Navbar.Collapse>
-          {collapseItems.map((item, index) => (
-            <Navbar.CollapseItem
-              key={item.name}
-              activeColor="primary"
-              css={{
-                color: index === collapseItems.length - 1 ? "$error" : "",
-              }}
-              isActive={index === 2}
-            >
-              <Link
-                color="inherit"
-                css={{
-                  minWidth: "100%",
-                }}
-                href={item.href}
-              >
-                {item.name}
-              </Link>
-            </Navbar.CollapseItem>
-          ))}
-        </Navbar.Collapse>
       </Navbar>
     </>
   );
