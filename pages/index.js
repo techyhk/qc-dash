beb26cf812c6072476cd7a1f9954c16b8e6a2037
techyhk <kumar.himanshu@icloud.com>
2023-02-13 12:47:39 +0530

    updated HomePage - wider input bar - moved pptr api to crawler api - idented code

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { useUser } from "../lib/hooks";
 import Layout from "../components/layout/Layout";
 import Details from "../components/details";
 import axios from "axios";
-import { SSRProvider } from 'react-bootstrap';
+import { SSRProvider } from "react-bootstrap";
 
 let socialUrls = {};
 
@@ -30,67 +30,89 @@ const Home = () => {
       <Layout>
         {user && (
           <>
-            <Spacer y={4} />
-            <Text b h2>Enter Website</Text>
-            <div css={{ maxHeight: "100vh" }}>
-              <Input
-                readOnly={inputState === "disabled" ? true : false}
-                className="search"
-                width="50%"
-                placeholder="Search"
-                clearable={inputState === "disabled" ? false : true}
-                color="warning"
-                onChange={searchHandler}
-                value={search}
-                aria-label="Search"
-                rounded
-              />
-              <Spacer y={0.5} />
-              <Button
-                color="gradient"
-                ghost
-                rounded
-                disabled={inputState === "disabled" ? true : false}
-                onPress={async () => {
-                  console.log("searchData", search);
-                  if (search != "") {
-                    setEnable(false);
-                    setError(null);
-                    setData({});
-                    setLoading(true);
-                    setInputState("disabled");
-                    try {
-                      socialUrls = await axios.post(`/api/pptr`, {
-                        url: search,
-                      });
-                      setData(socialUrls.data);
-                      setEnable(true);
-                    } catch (err) {
-                      setError(err.response.data.error);
-                    } finally {
-                      setLoading(false);
-                      setInputState("enabled");
-                    }
-                  } else {
-                    setError("Please enter a valid url");
-                  }
-                }}
-              >
-                Search
-              </Button>
-
-              {loading ? <><Spacer y="5" /><Loading type="points-opacity" size="xl" /></> : null}
-              {error ? <><Spacer y="2" /><Text color="error" b>Error : {error}</Text></> : null}
-              {enable ? (
-                <Details
-                  site={search}
-                  data={data}
+            <div>
+              <Text b h2>
+                Enter Website
+              </Text>
+              <div css={{ maxHeight: "100vh" }}>
+                <Input
+                  readOnly={inputState === "disabled" ? true : false}
+                  className="search"
+                  width="50%"
+                  placeholder="Search"
+                  clearable={inputState === "disabled" ? false : true}
+                  color="warning"
+                  onChange={searchHandler}
+                  value={search}
+                  aria-label="Search"
+                  rounded
+                  size="lg"
                 />
-              ) : null}
+                <Spacer y={0.5} />
+                <Button
+                  auto
+                  color="gradient"
+                  ghost
+                  rounded
+                  disabled={inputState === "disabled" ? true : false}
+                  onPress={async () => {
+                    console.log("searchData", search);
+                    if (search != "") {
+                      setEnable(false);
+                      setError(null);
+                      setData({});
+                      setLoading(true);
+                      setInputState("disabled");
+                      try {
+                        let headersList = {
+                          Accept: "*/*",
+                          "Content-Type": "application/json",
+                        };
+
+                        let reqOptions = {
+                          url: `/api/crawler/${search}`,
+                          method: "GET",
+                          headers: headersList,
+                        };
+
+                        let response = await axios.request(reqOptions);
+                        setData(response.data);
+                        setEnable(true);
+                      } catch (err) {
+                        setError(err.response.data.error);
+                      } finally {
+                        setLoading(false);
+                        setInputState("enabled");
+                      }
+                    } else {
+                      setError("Please enter a valid url");
+                    }
+                  }}
+                >
+                  Search
+                </Button>
+
+                {loading ? (
+                  <>
+                    <Spacer y="5" />
+                    <Loading type="points-opacity" size="xl" />
+                  </>
+                ) : null}
+                {error ? (
+                  <>
+                    <Spacer y="2" />
+                    <Text color="error" b>
+                      Error : {error}
+                    </Text>
+                  </>
+                ) : null}
+                {enable ? <Details site={search} data={data} /> : null}
+              </div>
             </div>
-          </>)}
+          </>
+        )}
       </Layout>
-    </SSRProvider >
+    </SSRProvider>
   );
 };
 
